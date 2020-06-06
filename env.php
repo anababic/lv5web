dccc656a113e55868d2d94c3a1f8252e06e44ebf
Ana Babic <ababic@etfos.hr>
2020-06-06 18:50:02 +0200

    Update env.php

--- env.php
+++ env.php
@@ -3,8 +3,8 @@ namespace db;
 
 class DbConfig
 {
-    const HOST = "localhost";
-    const USER = "root";
-    const PASS = "";
-    const DB = "lv5";
+    const HOST = "eu-cdbr-west-03.cleardb.net";
+    const USER = "bfc417c307b515";
+    const PASS = "65b7eddb";
+    const DB = "heroku_21c39846f08bffd";
 }
